a9095d9aaf14e3a7e3ac8a7f4ac03910f328f57f
breavedev <scott.brown0629@gmail.com>
2024-02-19 15:38:07 +0100

    Added Azuki sample contracts

--- contracts/DecaFT.sol
+++ contracts/DecaFT.sol
@@ -6,10 +6,12 @@ import "@layerzerolabs/solidity-examples/contracts/token/oft/v1/OFT.sol";
 
 // @dev example implementation inheriting a OFT
 contract DecaFT is OFT {
-    constructor(address _layerZeroEndpoint) OFT("DecaFT", "DFT", _layerZeroEndpoint) {}
+    constructor(uint256 initialSupply, address _layerZeroEndpoint) OFT("DecaFT", "DFT", _layerZeroEndpoint) {
+        _mint(msg.sender, initialSupply);
+    }
 
     // @dev WARNING public mint function, do not use this in production
-    function mint(address _to, uint256 _amount) external {
+    function mint(address _to, uint256 _amount) external onlyOwner() {
         _mint(_to, _amount);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -51,7 +51,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.4",
+        version: "0.8.18",
         settings: {
           optimizer: {
             enabled: true,
